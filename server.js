71e830231fe2ac7d8391cdcc6d66c73acc4cb6d7
Justin Isaf <jisaf@sapient.com>
2019-07-25 10:46:25 -0400

    comment out garbage

--- server.js
+++ server.js
@@ -1,111 +1,111 @@
-var express = require('express');
-var app = express();
-var BlinkDiff = require('blink-diff');
-// var urlToImage = require('url-to-image');
-var webshot = require('webshot');
-var sizeOf = require('image-size');
+// var express = require('express');
+// var app = express();
+// var BlinkDiff = require('blink-diff');
+// // var urlToImage = require('url-to-image');
+// var webshot = require('webshot');
+// var sizeOf = require('image-size');
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
-
-app.use('/images', express.static(__dirname + '/images'));
-// var diff = new BlinkDiff({
-//   imageAPath: '../images/good.png', // Use file-path
-//   imageBPath: '../images/bad.png',
-
-//   thresholdType: BlinkDiff.THRESHOLD_PERCENT,
-//   threshold: 0.01, // 1% threshold
-
-//   imageOutputPath: '../images/out.png'
+// app.listen(3000, () => {
+//   console.log('Server running on port 3000');
 // });
 
-var urlDiff = function(invision, production, callback) {
-  var options = {
-    // screenSize: {
-    //   width: 'all',
-    //   height: 'all'
-    // },
-    // shotSize: {
-    //   width: 'all',
-    //   height: 'all'
-    // },
-    renderDelay: 3000,
-  };
-  return webshot(invision, './images/good.png', options, function(err) {
-    // var imgSize = sizeOf('./images/good.png');
-    var options = {
-      // screenSize: {
-      //   // width: imgSize.width,
-      //   // height: imgSize.height
-      // },
-      // shotSize: {
-      //   height: 'all'
-      // },
-      renderDelay: 3000,
-    };
-    webshot(production, './images/bad.png', options, function(err) {
-      setTimeout(function() {
-        var diff = new BlinkDiff({
-        imageAPath: './images/good.png', // Use file-path
-        imageBPath: './images/bad.png',
-        // composition: false,
-        outputMaskRed: 0,
-        outputMaskBlue: 255, // Use blue for highlighting differences
-        thresholdType: BlinkDiff.THRESHOLD_PERCENT,
-        threshold: 0.01, // 1% threshold
-        imageOutputPath: './images/out.png'
-      });
-      callback(diff);
-    }, 100)
-      // now google.png exists and contains screenshot of google.com
-    });
-  });
-};
+// app.use('/images', express.static(__dirname + '/images'));
+// // var diff = new BlinkDiff({
+// //   imageAPath: '../images/good.png', // Use file-path
+// //   imageBPath: '../images/bad.png',
 
-// app.get('/compare', (req, res, next) => {
-//   diff.run(function(error, result) {
-//     if (error) {
-//       res.send('fail');
-//       throw error;
-//     } else {
-//       console.log(result);
-//       console.log(diff.hasPassed(result.code) ? 'Passed' : 'Failed');
-//       console.log('Found ' + result.differences + ' differences.');
-//       res.sendFile('/Users/jusisaf/dev/personal/VisualQA/images/out.png');
-//     }
+// //   thresholdType: BlinkDiff.THRESHOLD_PERCENT,
+// //   threshold: 0.01, // 1% threshold
+
+// //   imageOutputPath: '../images/out.png'
+// // });
+
+// var urlDiff = function(invision, production, callback) {
+//   var options = {
+//     // screenSize: {
+//     //   width: 'all',
+//     //   height: 'all'
+//     // },
+//     // shotSize: {
+//     //   width: 'all',
+//     //   height: 'all'
+//     // },
+//     renderDelay: 3000,
+//   };
+//   return webshot(invision, './images/good.png', options, function(err) {
+//     // var imgSize = sizeOf('./images/good.png');
+//     var options = {
+//       // screenSize: {
+//       //   // width: imgSize.width,
+//       //   // height: imgSize.height
+//       // },
+//       // shotSize: {
+//       //   height: 'all'
+//       // },
+//       renderDelay: 3000,
+//     };
+//     webshot(production, './images/bad.png', options, function(err) {
+//       setTimeout(function() {
+//         var diff = new BlinkDiff({
+//         imageAPath: './images/good.png', // Use file-path
+//         imageBPath: './images/bad.png',
+//         // composition: false,
+//         outputMaskRed: 0,
+//         outputMaskBlue: 255, // Use blue for highlighting differences
+//         thresholdType: BlinkDiff.THRESHOLD_PERCENT,
+//         threshold: 0.01, // 1% threshold
+//         imageOutputPath: './images/out.png'
+//       });
+//       callback(diff);
+//     }, 100)
+//       // now google.png exists and contains screenshot of google.com
+//     });
+//   });
+// };
+
+// // app.get('/compare', (req, res, next) => {
+// //   diff.run(function(error, result) {
+// //     if (error) {
+// //       res.send('fail');
+// //       throw error;
+// //     } else {
+// //       console.log(result);
+// //       console.log(diff.hasPassed(result.code) ? 'Passed' : 'Failed');
+// //       console.log('Found ' + result.differences + ' differences.');
+// //       res.sendFile('/Users/jusisaf/dev/personal/VisualQA/images/out.png');
+// //     }
+// //   });
+// // });
+
+// app.post('/url', (req, res, next) => {
+//   console.log(req.query);
+//   // const url = 'https://publicis.invisionapp.com/share/5XT1S7SZS8V#/screens/374597737_Side_Drawer';
+//   // const url = req.query.input;
+
+//   // axios(url)
+//   // .then(response => {
+//   // const html = response.data;
+//   // console.log(html)
+//   // const $ = cheerio.load(html);
+//   // const imageUrl = $('#screen_viewer img').attr('src')
+//   // var regex = /(https?:\/\/.+invisionapp-cdn.+)/ig;
+//   // console.log(html.match(regex))
+//   // console.log(imageUrl);
+//   urlDiff(req.query.input, req.query.output, function(diff) {
+//     diff.run(function(error, result) {
+//       if (error) {
+//         res.send('fail');
+//         throw error;
+//       } else {
+//         console.log(diff.hasPassed(result.code) ? 'Passed' : 'Failed');
+//         console.log('Found ' + result.differences + ' differences.');
+//         res.sendFile(
+//           '/Users/jusisaf/dev/personal/VisualQA/images/out.png'
+//         );
+//       }
+//     });
 //   });
 // });
-
-app.post('/url', (req, res, next) => {
-  console.log(req.query);
-  // const url = 'https://publicis.invisionapp.com/share/5XT1S7SZS8V#/screens/374597737_Side_Drawer';
-  // const url = req.query.input;
-
-  // axios(url)
-  // .then(response => {
-  // const html = response.data;
-  // console.log(html)
-  // const $ = cheerio.load(html);
-  // const imageUrl = $('#screen_viewer img').attr('src')
-  // var regex = /(https?:\/\/.+invisionapp-cdn.+)/ig;
-  // console.log(html.match(regex))
-  // console.log(imageUrl);
-  urlDiff(req.query.input, req.query.output, function(diff) {
-    diff.run(function(error, result) {
-      if (error) {
-        res.send('fail');
-        throw error;
-      } else {
-        console.log(diff.hasPassed(result.code) ? 'Passed' : 'Failed');
-        console.log('Found ' + result.differences + ' differences.');
-        res.sendFile(
-          '/Users/jusisaf/dev/personal/VisualQA/images/out.png'
-        );
-      }
-    });
-  });
-});
 
 
 var express = require('express');
